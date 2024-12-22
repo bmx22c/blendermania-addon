99cafb4fb6a56e2c3ea3789cf4b697bc3ada046b
bmx22c <github@bmx22c.fr>
2024-12-22 12:13:55 +0100

    disabled NICE to prevent issues

--- __init__.py
+++ __init__.py
@@ -80,7 +80,7 @@ from .panels.PT_Textures                import *
 from .panels.PT_VisibilitySelection     import *
 from .panels.PT_EditorTrails            import *
 
-from .NICE.nice import NICE_register, NICE_unregister
+# from .NICE.nice import NICE_register, NICE_unregister
 
 
 # owner of the object eventlistener
@@ -302,7 +302,7 @@ def register():
     bpy.app.handlers.depsgraph_update_post.append(listen_object_move)
     bpy.types.Object.location_before = FloatVectorProperty(name="old pos", subtype="TRANSLATION")
 
-    NICE_register()
+    # NICE_register()
     
 
 
